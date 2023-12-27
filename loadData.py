0ee91231a30dd58efbc8ac6afac0518dcfd9d64a
Phillip Passmore <phillipspassmore@gmail.com>
2023-12-27 12:27:20 +0000

    fixed conflicting file name
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- loadData.py
+++ loadData.py
@@ -1,11 +1,11 @@
-import secrets
+import secrets_store
 import requests
 import pandas as pd
 
 class dataSetUp:
-    def __init__(self, api_key, steam_id) -> None:
-        self.api_key = secrets.steamKey
-        self.steam_id = secrets.userID
+    def __init__(self) -> None:
+        self.api_key = secrets_store.steamKey
+        self.steam_id = secrets_store.userID
     
     def getOwnedGames(self):
         url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={self.api_key}&steamid={self.steam_id}&include_appinfo=1&include_played_free_games=1&format=json'
@@ -22,7 +22,7 @@ class dataSetUp:
                     playtime_2weeks = game.get('playtime_2weeks', 0)
                     playtime_forever = game.get('playtime_forever', 0)
                     img_icon_url = game.get('img_icon_url')
-                    img_logo_url = game.get('img_logo_url')
+                    
 
                     # Append game data to the list
                     game_data.append({
@@ -31,14 +31,13 @@ class dataSetUp:
                         'Playtime (2 weeks)': playtime_2weeks,
                         'Playtime (forever)': playtime_forever,
                         'Icon URL': img_icon_url,
-                        'Logo URL': img_logo_url
+                       
                     })
 
                 # Create a DataFrame from the list of game data
                 df = pd.DataFrame(game_data)
 
-                # Print or further process the DataFrame
-                print(df)
+                return df
             else:
                 print("No games found in the library.")
         else:
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
-import secrets
+import secrets_store
 import requests
 import loadData
 import pandas as pd
 
-api_key = secrets.steamKey
-steam_id = secrets.userID
+api_key = secrets_store.steamKey
+steam_id = secrets_store.userID
 
-loadData.dataSetUp(api_key, steam_id)
+data_setup = loadData.dataSetUp()
 
 '''
 url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steam_id}&include_appinfo=1&include_played_free_games=1&format=json'
@@ -45,12 +45,24 @@ if response.status_code == 200:
             playtime_2weeks = game.get('playtime_2weeks', 0)
             playtime_forever = game.get('playtime_forever', 0)
             img_icon_url = game.get('img_icon_url')
-            img_logo_url = game.get('img_logo_url')
 
             print(f"Game ID: {appid}, Name: {name}, Playtime (2 weeks): {playtime_2weeks} minutes, Playtime (forever): {playtime_forever} minutes")
-            #print(f"Icon URL: {img_icon_url}, Logo URL: {img_logo_url}\n")
+            
     else:
         print("No games played recently.")
 else:
     print(f"Error: {response.status_code}, {response.text}")
+
+
+
+df = data_setup.getOwnedGames()
+print(df)
+
+csv_filename = 'owned_games.csv'  # Specify the desired filename
+df.to_csv(csv_filename, index=False)
+
+zero_playtime_count = df['Playtime (forever)'] == 0
+zero_playtime_games = df[zero_playtime_count]
+
+print(f"Number of games with zero playtime: {len(zero_playtime_games)}")
 
